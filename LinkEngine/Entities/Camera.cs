402d36ef6e5a4058e1b5f2c230f1dddfb7f1ace9
ThinkLink99 <linkin562@gmail.com>
2018-06-29 08:51:22 -0400

    work commit

--- LinkEngine/Entities/Camera.cs
+++ LinkEngine/Entities/Camera.cs
@@ -7,6 +7,13 @@ namespace LinkEngine.Entities
         public int X { get; set; }
         public int Y { get; set; }
 
+        public Point[] Boundries { get; set; }
+
         public Image View { get; set; }
+
+        public Camera ()
+        {
+            Boundries = new Point[2];
+        }
     }
 }
---
+++ LinkEngine/GameTypes/Top-Down/Strategy/Turn-Based/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkEngine.GameTypes.Top_Down.Strategy.Turn_Based
+{
+    public class Player
+    {
+        /// <summary>
+        /// The Faction is who the player will play as ingame
+        /// </summary>
+        public Faction Faction { get; set; }
+
+        /// <summary>
+        /// The camera controlling how the player see the world
+        /// </summary>
+        public Entities.Camera camera { get; set; }
+    }
+}
