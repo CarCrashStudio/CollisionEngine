6e0ec4315165f583c19315f12f6105bbfb53d2ad
Trey Hall <linkin562@gmail.com>
2018-08-01 11:08:29 -0400

    removed all extra folders not directly apart of linkengine

--- Turn-BasedStrategy/Engine/Leader.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LinkEngine.Strategy.TurnBased
-{
-    public class Leader
-    {
-        public string Title { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
--- Turn-BasedStrategy/Engine/Territory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LinkEngine.Strategy.TurnBased
-{
-    public class Territory
-    {
-        public string Name { get; set; }
-    }
-}
